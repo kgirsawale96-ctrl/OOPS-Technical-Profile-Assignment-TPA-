d5fe74684c1c82723464541e557a6fac085bb58b
Kalyani <kgirsawale96@gmail.com>
2026-03-17 01:07:32 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person p1 = new Person("Rahul", 20);
+        Person p2 = new Person("Priya", 22);
+
+        p1.display();
+        System.out.println();
+        p2.display();
+    }
+}
